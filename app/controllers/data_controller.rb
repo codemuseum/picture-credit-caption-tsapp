298d2bf33a6da4b812e993141299bd569aee8d51
Matthew Moore <matt@thrivesmart.com>
2009-01-16 14:57:22 -1000

    data controller update

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -3,7 +3,7 @@ class DataController < ApplicationController
   # GET /data.xml
   def index
     @page_objects = PageObject.find(:all, :conditions => { :urn => params[:from] })
-    @data = @page_objects.collect { |po| { :urn => po.urn } }
+    @data = @page_objects.collect { |po| { :urn => po.urn, :picture => po.picture_url } }
     
     respond_to do |format|
       format.xml  { render :xml => @data }
