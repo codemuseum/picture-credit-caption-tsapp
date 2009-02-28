cf70a095976b8a2ee0afb8eeec5c121a65eaa202
Matthew Moore <matt@thrivesmart.com>
2009-02-27 14:38:58 -1000

    using new tsapp rails version

--- app/models/page_object.rb
+++ app/models/page_object.rb
@@ -1,7 +1,8 @@
 class PageObject < ActiveRecord::Base
   include ThriveSmartObjectMethods
 
-  self.caching_default = :page_object_update #[in :forever, :page_object_update, :any_page_object_update, 'data_update[datetimes]', :never, 'interval[5]']
+  self.caching_default = :page_object_update 
+  #[in :forever, :page_object_update, :any_page_object_update, 'data_update[datetimes]', :never, 'interval[5]']
   
   def picture?
     ThriveSmart::Helpers::AssetHelper::asset?(asset_type, asset_urn)
